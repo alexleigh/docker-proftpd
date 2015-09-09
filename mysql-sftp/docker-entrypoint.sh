ebabcbf22af46ddfa403570a22d19134f55daedd
Alex Leigh <leigh@alexleigh.me>
2015-09-09 14:57:22 -0700

    Add mysql-sftp tag.

---
+++ mysql-sftp/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cat > /etc/proftpd/sql.conf <<EOH
+<Global>
+<IfModule mod_sql.c>
+SQLBackend mysql
+SQLEngine on
+SQLAuthenticate users userset
+SQLAuthTypes Crypt
+SQLConnectInfo ${DB_NAME}@${DB_HOST} ${DB_USER} ${DB_PASS}
+SQLDefaultUID $(id -u ftp)
+SQLDefaultGID $(id -g ftp)
+</IfModule>
+</Global>
+EOH
+
+cat > /etc/proftpd/sftp.conf <<EOH
+<IfModule mod_sftp.c>
+<VirtualHost $(hostname)>
+Port 23
+SFTPEngine on
+SFTPAuthMethods password
+SFTPOptions IgnoreSFTPSetPerms IgnoreSFTPUploadPerms IgnoreSFTPSetTimes
+SFTPHostKey /etc/ssh/ssh_host_dsa_key
+SFTPHostKey /etc/ssh/ssh_host_rsa_key
+SFTPHostKey /etc/ssh/ssh_host_ecdsa_key
+</VirtualHost>
+</IfModule mod_sftp.c>
+EOH
+
+exec /usr/sbin/proftpd --nodaemon
